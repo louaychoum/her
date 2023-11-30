a90a6e26c3472d4d27edf3faa07f1fe83430838e
Louay Choum <louay@verofax.com>
2023-11-30 12:45:00 +0300

    Fix formatting and language in config.js and index.html

--- config.js
+++ config.js
@@ -2,10 +2,11 @@ var config = {
     lines: [
         "Hey albi, get well soon, but no rush.",
         "Netflix and chill, but just the chill part.",
-        "You, in bed, me playing doc,",
-        "Who knew chicken soup could be an art?",
+        "You, in bed, me playing doc.",
+        "I'll take care of you bum",
+        "I like you alot Yasmin",
+        "You're my favourite person",
+        "You're my favourite smile",
     ],
-    date: "2023-11-30T12:15:00",
-    // 双方的名字
-    names: ["Yasmin",]
+    names: ["Yasmin"]
 }
