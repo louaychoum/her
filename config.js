071f47be705e8201be310747071b76ca836e844d
Louay Choum <louay@verofax.com>
2023-11-30 12:23:50 +0300

    Update index.html with new content

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    lines: [
+        "Hey albi, get well soon, but no rush.",
+        "Netflix and chill, but just the chill part.",
+        "You, in bed, me playing doc,",
+        "Who knew chicken soup could be an art?",
+    ],
+    date: "2023-11-30T12:15:00",
+    // 双方的名字
+    names: ["Yasmin",]
+}
